886c4f31a976ac6071d279650d3d841ce8af1a1e
Martn9 <tuco92276@gmail.com>
2026-05-07 12:22:13 -0400

    docs: Finalizar base arquitectónica y API para el equipo

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Restaurante, Producto
+
+admin.site.register(Restaurante)
+admin.site.register(Producto)
